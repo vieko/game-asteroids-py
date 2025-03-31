49e9a33182a4ed0c5aed34a9e8291f3ce6d1ff07
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2025-03-30 18:28:28 -0600

    adds player to screen with basic rotation

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import pygame
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
+
 
 def main():
-    print('Starting Asteroids!')
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+    print("Starting Asteroids!")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-                
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
 
-if __name__ == '__main__':
+        screen.fill((0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+if __name__ == "__main__":
     main()
